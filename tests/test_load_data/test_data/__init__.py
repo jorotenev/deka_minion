c87b9dd0e77f973f56472a3187070042c208e0db
Georgi Tenev <georgetenevbg@gmail.com>
2018-04-25 21:47:39 +0300

    [load_data] added a test to ensure that loading multiple areas would work as expected. Refactored the tests to extract commonly used checks in their own class

--- tests/test_load_data/test_data/__init__.py
+++ tests/test_load_data/test_data/__init__.py
@@ -2,4 +2,5 @@ import json
 
 from shared_utils.file_utils import get_directory_of_file
 
-dummy_data = json.load(open("%s/sofia_short.json" % get_directory_of_file(__file__)))
+dummy_data_sofia = json.load(open("%s/sofia.json" % get_directory_of_file(__file__)))
+dummy_data_leuven = json.load(open("%s/leuven.json" % get_directory_of_file(__file__)))
--- tests/test_load_data/test_redis_adapter.py
+++ tests/test_load_data/test_redis_adapter.py
@@ -4,60 +4,31 @@ from uuid import uuid4
 
 from load_data.datastore_adapter import load_to_datastore, Facade
 from load_data.datastore_adapter.redis import r, cities_boundaries_template_key, cities_places_template_key, \
-    cities_coordinates_template_key, KeyConverter
+    cities_coordinates_template_key
 from load_data.main import parse_raw_input
-from tests.test_load_data.test_data import dummy_data
+from tests.test_load_data.test_data import dummy_data_sofia, dummy_data_leuven
 from . import test_redis_db
 
 
-def drop_db(redis_client):
-    [redis_client.delete(key) for key in redis_client.keys("*")]
-
-
-def ensure_correct_redis_used():
-    # we want to verify that we are using a testing db namespace, to avoid polluting the development one
-    # we determine that we are connected to the correct namespace by setting a dummy value,
-    # then calling "info" on the redis client.
-    # this works because the Config is altered in the __init__.py file :)
-    dummy = str(uuid4())
-    r.set(dummy, 1)
-    assert 'db%i' % test_redis_db in r.info("keyspace"), \
-        "The redis client is not using the testing db namespace. Testing could corrupt development data"
-
-    # clean-up
-    drop_db(r)
-
-    assert len(r.keys("*")) == 0, "There are %i keys" % len(r.keys("*"))
-    # good to go
-
-
-class TestRedisAdapter(TestCase):
+class TestRedisMixin(TestCase):
     @classmethod
     def setUpClass(cls):
-        ensure_correct_redis_used()
-        cls.places, cls.metadata = parse_raw_input(dummy_data)
+        ensure_correct_redis_db_during_testing()
+        # read data
+        cls.places_sofia, cls.metadata_sofia = parse_raw_input(dummy_data_sofia)
 
     def tearDown(self):
         drop_db(r)
 
+
+class TestRedisAdapter(TestRedisMixin):
+
     def test_correct_top_level_keys(self):
         # dummy_data was loaded from a file with the same format as original data.
-        load_to_datastore(self.places, self.metadata)
-        TestRedisAdapter.assert_correct_top_level_keys_loaded_in_redis(tester=self, metadata=self.metadata)
-
-    @staticmethod
-    def assert_correct_top_level_keys_loaded_in_redis(tester, metadata):
-        """
-        :param tester: `self` from a method in class extending TestCase
-        :param metadata: see the load_data readme
-        """
-        area_name = metadata.area_name
-        expected_keys = [
-            "%s%s" % (cities_boundaries_template_key, area_name),
-            "%s%s" % (cities_places_template_key, area_name),
-            "%s%s" % (cities_coordinates_template_key, area_name)
-        ]
-        tester.assertEqual(set(expected_keys), set(r.keys("*")))
+        load_to_datastore(self.places_sofia, self.metadata_sofia)
+        CommonAssertions.check_exclusive_correct_top_level_keys_loaded_in_redis(tester=self,
+                                                                                expected_areas=[
+                                                                                    self.metadata_sofia.area_name])
 
     def test_correct_data_under_boundaries(self):
         """
@@ -68,29 +39,15 @@ class TestRedisAdapter(TestCase):
             "southeast":{"lat":1, "lng":2},
         }
         """
-        load_to_datastore(self.places, self.metadata)
-        TestRedisAdapter.assert_correct_data_under_boundaries(tester=self, metadata=self.metadata)
+        load_to_datastore(self.places_sofia, self.metadata_sofia)
 
-    @staticmethod
-    def assert_correct_data_under_boundaries(tester, metadata):
-        area_name = metadata.area_name
+        area_name = self.metadata_sofia.area_name
 
-        expected_cities_boundaries_keys = [area_name]
-        raw_keys = r.keys("*%s*" % cities_boundaries_template_key)
+        # ensure only the boundary for the correct single area is loaded
+        redis_boundaries_keys = r.keys("*%s*" % cities_boundaries_template_key)  # returns a list
+        self.assertEqual(set([cities_boundaries_template_key + area_name]), set(redis_boundaries_keys))
 
-        # keys == ["cities:boundaries:london"]
-        keys = [KeyConverter.get_area_name(k) for k in raw_keys]
-        tester.assertEqual(set(keys), set(expected_cities_boundaries_keys))
-
-        expected_cities_boundaries_value = json.dumps(
-            {
-                "northwest": {"lat": metadata.bounding_rectangle["northwest"]["lat"],
-                              "lng": metadata.bounding_rectangle["northwest"]["lng"]},
-                "southeast": {"lat": metadata.bounding_rectangle["southeast"]["lat"],
-                              "lng": metadata.bounding_rectangle["southeast"]["lng"]},
-            }
-        )
-        tester.assertEqual(expected_cities_boundaries_value, r.get(cities_boundaries_template_key + area_name))
+        CommonAssertions.check_correct_boundaries_for_area(tester=self, metadata=self.metadata_sofia)
 
     def test_correct_data_under_places(self):
         """
@@ -98,11 +55,122 @@ class TestRedisAdapter(TestCase):
         The key is a place_id as per the Google Places Search API.
         The value is an object, as returned by the same API.
         """
-        load_to_datastore(self.places, self.metadata)
-        TestRedisAdapter.assert_correct_data_under_places(tester=self, places=self.places, metadata=self.metadata)
+        load_to_datastore(self.places_sofia, self.metadata_sofia)
+        CommonAssertions.check_correct_data_under_places(tester=self, places=self.places_sofia,
+                                                         metadata=self.metadata_sofia)
+
+    def test_correct_data_under_coordinates(self):
+        """
+        https://redis.io/commands/geoadd
+        The `cities_coordinates_template_key<area_name>` holds a sorted set.
+        Here we test that the correct items were added to this set.
+        An item consists of [lat lng place_id], where place_id is also a key under the
+        `cities_places_template_key<area_name>`
+        """
+        load_to_datastore(self.places_sofia, self.metadata_sofia)
+        CommonAssertions.check_correct_data_under_coordinates(tester=self, metadata=self.metadata_sofia,
+                                                              places=self.places_sofia)
+
+
+class TestUpdatingExistingArea(TestRedisMixin):
+    """
+    Make sure that updating existing area works (e.g. we've already loaded data for sofia, and we want to update it)
+    The test below is actually just loading some data, running all tests from TestRedisAdapter (sanity checking), then loading
+    another dataset and running the same sets again.
+    """
+
+    def test_update(self):
+        """
+        We'd first load half of the dummy data for the given city.
+        We'd then load the whole data set.
+        We expect that the old data is fully replaced with the new one
+        """
+        metadata = self.metadata_sofia  # the metadata'd stay the same
+        places_initial_batch = dict(
+            list(self.places_sofia.items())[:len(self.places_sofia) // 2])  # get the first half of a dict
+        load_to_datastore(places=places_initial_batch, metadata=metadata)
+
+        # sanity checking
+        CommonAssertions.run_all_tests_single_area_loaded(tester=self, places=places_initial_batch, metadata=metadata)
+
+        # now load a second batch (which is just all of the available data)
+        load_to_datastore(places=self.places_sofia, metadata=metadata)
+        # and test again
+        CommonAssertions.run_all_tests_single_area_loaded(tester=self, places=self.places_sofia, metadata=metadata)
+
+
+class TestMultipleGeoAreas(TestRedisMixin, ):
+    @classmethod
+    def setUpClass(cls):
+        super(TestMultipleGeoAreas, cls).setUpClass()
+        cls.places_leuven, cls.metadata_leuven = parse_raw_input(dummy_data_leuven)
+
+    def test_load_two_areas(self):
+        data = [
+            [self.metadata_sofia, self.places_sofia],
+            [self.metadata_leuven, self.places_leuven]
+        ]
+
+        for metadata, places in data:
+            print("loading data for %s" % metadata.area_name)
+            load_to_datastore(places=places, metadata=metadata)
+
+        for metadata, places in data:
+            print("run all tests for %s" % metadata.area_name)
+            CommonAssertions.run_all_tests_single_area_loaded(tester=self, places=places, metadata=metadata)
+            CommonAssertions.check_exclusive_correct_top_level_keys_loaded_in_redis(tester=self, expected_areas=[
+                self.metadata_leuven.area_name,
+                self.metadata_sofia.area_name,
+            ])
+
+
+class CommonAssertions:
+    """
+    the methods don't make the assumption that there's a single area loaded - e..g. they work even if more than
+    one area are loaded in redis
+    """
 
     @staticmethod
-    def assert_correct_data_under_places(tester, places, metadata):
+    def run_all_tests_single_area_loaded(tester, places, metadata):
+        # test all data in redis for a single area.
+
+        CommonAssertions.check_correct_data_under_coordinates(tester=tester, places=places, metadata=metadata)
+        CommonAssertions.check_correct_boundaries_for_area(tester=tester, metadata=metadata)
+        CommonAssertions.check_correct_data_under_places(tester=tester, places=places, metadata=metadata)
+
+    @staticmethod
+    def check_exclusive_correct_top_level_keys_loaded_in_redis(tester: TestCase, expected_areas):
+        """
+        only the keys for the selected areas are allowed. e.g. if for a given area there're extra keys, this
+        check will bark
+        """
+        expected_keys = []
+        for area_name in expected_areas:
+            # we expect each of the keys below for each area
+            expected_keys.append("%s%s" % (cities_boundaries_template_key, area_name))
+            expected_keys.append("%s%s" % (cities_places_template_key, area_name))
+            expected_keys.append("%s%s" % (cities_coordinates_template_key, area_name))
+
+        tester.assertEqual(set(expected_keys), set(r.keys("*")))
+
+    @staticmethod
+    def check_correct_boundaries_for_area(tester, metadata):
+        area_name = metadata.area_name
+        expected_boundary_rectangle = json.dumps(
+            {
+                "northwest": {"lat": metadata.bounding_rectangle["northwest"]["lat"],
+                              "lng": metadata.bounding_rectangle["northwest"]["lng"]},
+                "southeast": {"lat": metadata.bounding_rectangle["southeast"]["lat"],
+                              "lng": metadata.bounding_rectangle["southeast"]["lng"]},
+            }
+        )
+
+        loaded_value = r.get(cities_boundaries_template_key + area_name)
+
+        tester.assertEqual(expected_boundary_rectangle, loaded_value)
+
+    @staticmethod
+    def check_correct_data_under_places(tester, places, metadata):
 
         area_name = metadata.area_name
         places_key_for_area = cities_places_template_key + area_name
@@ -118,72 +186,42 @@ class TestRedisAdapter(TestCase):
             stored_place = Facade.get_place_data(area_name=area_name, place_key=place_key)
             tester.assertEqual(places[place_key], stored_place)
 
-    def test_correct_data_under_coordinates(self):
-        """
-        https://redis.io/commands/geoadd
-        The `cities_coordinates_template_key<area_name>` holds a sorted set.
-        Here we test that the correct items were added to this set.
-        An item consists of [lat lng place_id], where place_id is also a key under the
-        `cities_places_template_key<area_name>`
-        """
-        load_to_datastore(self.places, self.metadata)
-        TestRedisAdapter.assert_correct_data_under_coordinates(tester=self, metadata=self.metadata, places=self.places)
-
     @staticmethod
-    def assert_correct_data_under_coordinates(tester, places, metadata):
+    def check_correct_data_under_coordinates(tester, places, metadata):
         area_name = metadata.area_name
 
         # for each place we've loaded query the datastore using the lat+lng of the given place.
         # the query is within a given small radius (for a lack of a better way to search)
-        # we expected that the result will be a single item, representing the given place
+        # we expected that the result will be one (or at least just a very few) place
         for place_id, place in places.items():
             # use the coordinates of a place to query the datastore within a radius
-            items = r.georadius(cities_coordinates_template_key + area_name,
-                                latitude=place['geometry']['location']['lat'],
-                                longitude=place['geometry']['location']['lng'],
-                                radius=1,
-                                unit='m')
-            tester.assertEqual(1, len(items), "Expect that the datastore returned a single"
-                                              "items when querying for a location")
-            tester.assertEqual(place['place_id'], items[0])
+            db_items_ids = r.georadius(cities_coordinates_template_key + area_name,
+                                       latitude=place['geometry']['location']['lat'],
+                                       longitude=place['geometry']['location']['lng'],
+                                       radius=1,
+                                       unit='m')
+            tester.assertTrue(len(db_items_ids) >= 1,
+                              "Expecteed at least one item when querying a location for which a place have been added")
+
+            tester.assertIn(place['place_id'], db_items_ids)
 
 
-class TestUpdatingExistingArea(TestCase):
-    """
-    Make sure that updating existing area works (e.g. we've already loaded data for sofia, and we want to update it)
-    The test below is actually just loading some data, running all tests from TestRedisAdapter (sanity checking), then loading
-    another dataset and running the same sets again.
-    """
+def drop_db(redis_client):
+    [redis_client.delete(key) for key in redis_client.keys("*")]
 
-    @classmethod
-    def setUpClass(cls):
-        ensure_correct_redis_used()
-        cls.places, cls.metadata = parse_raw_input(dummy_data)
 
-    def tearDown(self):
-        drop_db(r)
+def ensure_correct_redis_db_during_testing():
+    # we want to verify that we are using a testing db namespace, to avoid polluting the development one
+    # we determine that we are connected to the correct namespace by setting a dummy value,
+    # then calling "info" on the redis client.
+    # this works because the Config is altered in the __init__.py file :)
+    dummy = str(uuid4())
+    r.set(dummy, 1)
+    assert 'db%i' % test_redis_db in r.info("keyspace"), \
+        "The redis client is not using the testing db namespace. Testing could corrupt development data"
 
-    @staticmethod
-    def run_all_tests(tester, places, metadata):
-        # test all data in redis
-        TestRedisAdapter.assert_correct_data_under_coordinates(tester=tester, places=places, metadata=metadata)
-        TestRedisAdapter.assert_correct_data_under_boundaries(tester=tester, metadata=metadata)
-        TestRedisAdapter.assert_correct_data_under_places(tester=tester, places=places, metadata=metadata)
+    # clean-up
+    drop_db(r)
 
-    def test_update(self):
-        """
-        We'd first load half of the dummy data for the given city.
-        We'd then load the whole data set.
-        We expect that the old data is fully replaced with the new one
-        """
-        metadata = self.metadata  # the metadata'd stay the same
-        places_initial_batch = dict(list(self.places.items())[:len(self.places) // 2])  # get the first half of a dict
-        load_to_datastore(places=places_initial_batch, metadata=metadata)
-
-        # sanity checking
-        TestUpdatingExistingArea.run_all_tests(tester=self, places=places_initial_batch, metadata=metadata)
-
-        # now load a second batch (which is just all of the available data)
-        load_to_datastore(places=self.places, metadata=metadata)
-        # and test again
-        TestUpdatingExistingArea.run_all_tests(tester=self, places=self.places, metadata=metadata)
+    assert len(r.keys("*")) == 0, "There are %i keys" % len(r.keys("*"))
+    # good to go
